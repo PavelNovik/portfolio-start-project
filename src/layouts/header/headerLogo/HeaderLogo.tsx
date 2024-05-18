f0c257bc9059cad597fbd27bd0061ecd444d1c87
Pavel Novik <brn2befree@gmail.com>
2024-05-18 14:52:35 +0300

    change header icon

--- src/layouts/header/headerLogo/HeaderLogo.tsx
+++ src/layouts/header/headerLogo/HeaderLogo.tsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import {MLink} from "../../../components/Link";
 import {HeaderIcon} from "./headerIcon/HeaderIcon";
+import {HIcon} from "./headerIcon/Hicon";
 
 export const HeaderLogo: React.FC = () => {
     return (
         <MLink href={"#"}>
-            <HeaderIcon/>
+            {/*<HeaderIcon/>*/}
+            <HIcon/>
         </MLink>
     );
 };
